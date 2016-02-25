8afe06f513b219dd39a0ce7babb4f7317cd51e9b
arunitgupta <arunitgupta@gmail.com>
2016-02-24 23:47:41 -0600

    commited

---
+++ Source_Lab5/login.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Arunit Gupta on 2/15/2016.
+ */
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "Admin@gmail.com" && password == "Admin"){
+        alert ("Login successfully");
+        window.location = "Home.html"; // Redirecting to other page.
+        return false;
+    }
+    else{
+        attempt --;// Decrementing by one.
+        alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+        if( attempt == 0){
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
